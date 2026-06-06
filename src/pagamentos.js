86d018b513296437cebb8ee61b61402b11c4caf9
Weslley <weslleycarvalho344@gmail.com>
2026-06-06 17:12:23 -0300

    ci: Adicionado pipeline

--- src/pagamentos.js
+++ src/pagamentos.js
@@ -18,7 +18,7 @@ export default class Pagamentos {
             } else {
                 this.#pagamentos[i].categoria = 'padrão';
             }
-        }
+        };
     }
 
     consultarUltimoPagamento() {
